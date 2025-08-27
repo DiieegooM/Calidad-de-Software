599bd6e94ea1fea3707f9fd86ba725a7a0254c1c
PV-Alumno <PV-Alumno@duoc.acad>
2025-08-27 18:43:30 -0400

    subir proyecto completo

---
+++ app/calculate.py
@@ -0,0 +1,8 @@
+import math
+
+def calcular_hipotenusa(cateto1, cateto2):
+
+    #Calcula la hipotenusa de un triángulo rectángulo dados los dos catetos.
+    
+    return math.sqrt(cateto1 ** 2 + cateto2 ** 2)
+
